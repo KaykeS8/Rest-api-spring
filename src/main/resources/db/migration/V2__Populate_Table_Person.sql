162c9d7f326a58f2ab05ac0dc989820f593c7fe8
Kayke simao <kayke.py@gmail.com>
2026-07-29 21:21:31 -0300

    Working with migration

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,12 @@
+INSERT INTO `persons` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+(17, 'Carlos', 'Silva', 'California - EUA', 'Homem'),
+(18, 'Mariana', 'Souza', 'São Paulo - SP', 'Mulher'),
+(20, 'Ana', 'Oliveira', 'Curitiba - PR', 'Mulher'),
+(21, 'Lucas', 'Fernandes', 'Porto Alegre - RS', 'Homem'),
+(23, 'Rafael', 'Gomes', 'Fortaleza - CE', 'Homem'),
+(24, 'Juliana', 'Martins', 'Recife - PE', 'Mulher'),
+(25, 'Pedro', 'Almeida', 'Brasília - DF', 'Homem'),
+(26, 'Camila', 'Barbosa', 'Florianópolis - SC', 'Mulher'),
+(27, 'Fernanda', 'bertnoli', 'Belo Horizonte - MG', 'Mulher'),
+(29, 'Fernando', 'amargo', 'Belo horizonte - MG', 'Homem'),
+(30, 'Fernando', 'amargo', 'Belo horizonte - MG', 'Homem');
